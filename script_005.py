c944402eabd6252fb6a3c27c3f69fa0fa81cf1a6
Selvaganapathi-A <selvaganapathi066@gmail.com>
2024-02-01 21:38:55 +0530

    Range Data
    Range data type in postgres

--- script_005.py
+++ script_005.py
@@ -9,30 +9,50 @@ def main():
             conninfo="dbname=postgres user=postgres password=Windows@11"
     ) as connection:
         with connection.cursor() as cursor:
-            # * Date/Time/Interval Data types
+            # * Array Data types
             # create table
-            cursor.execute(("create table if not exists demo_datetime("
-                            ""
+            cursor.execute(("create table if not exists demo_arrays("
+                            "pk smallserial,"
+                            "a integer[]"
                             ");"))
             # insert data
 
-            cursor.executemany(
-                "insert into demo_datetime(a,b,c,d,e,f) values (%s, %s, %s, %s, %s, %s);",
-                (),
+            cursor.execute(
+                "insert into demo_arrays(a) values (%s);",
+                ("{5, 8, 9, 10}", ),
             )
-            cursor.execute(query="select * from demo_datetime;")
+            cursor.execute(
+                "insert into demo_arrays(a) values (%s);",
+                ([924, 3, 7], ),
+            )
+            #
+            # ! add array of items to array
+            cursor.execute(
+                "update demo_arrays set a =  array_cat(a, %s) where pk = 2;",
+                ([49, 63, 70, 84, 140, 280], ),
+            )
+            #
+            # ! add single item to array
+            cursor.execute(
+                "update demo_arrays set a = array_append(a, %s) where pk = 1;",
+                (238, ),
+            )
+            #
+            cursor.execute(
+                query="select * from demo_arrays order by pk asc nulls last;")
             #
             results = cursor.fetchall()
             for result in results:
                 for x in result:
                     print(x)
+                    print(type(x))
                 print()
-                for x in result:
-                    print(f"{x!r}")
+                # for x in result:
+                #     print(f"{x!r}")
                 print()
             print()
             #
-            cursor.execute("drop table if exists demo_datetime;")
+            cursor.execute("drop table if exists demo_arrays;")
             #
         connection.commit()
         connection.close()
---
+++ script_006.py
@@ -0,0 +1,83 @@
+import datetime
+
+from psycopg.types.range import Range
+
+import psycopg
+import pytz
+
+# ! Postgresql Data types
+
+
+def main():
+    #
+    with psycopg.Connection.connect(
+            conninfo="dbname=postgres user=postgres password=Windows@11"
+    ) as connection:
+        with connection.cursor() as cursor:
+            # * Range Data type
+            # create table
+            #
+            #
+            cursor.execute(("select "
+                            "%s::int4range, "
+                            "%s::int8range, "
+                            "%s::int8range, "
+                            "%s::numrange, "
+                            "%s::numrange, "
+                            "%s::tsrange, "
+                            "%s::tstzrange, "
+                            "%s::daterange;"), (
+                                (0, 5),
+                                (6, 10),
+                                Range(lower=11),
+                                Range(21, 30),
+                                Range(upper=41),
+                                (
+                                    datetime.datetime(2020, 1, 1),
+                                    datetime.datetime(2020, 12, 31),
+                                ),
+                                (
+                                    datetime.datetime(
+                                        2021,
+                                        1,
+                                        1,
+                                        tzinfo=pytz.timezone("Asia/Calcutta"),
+                                    ),
+                                    datetime.datetime(
+                                        2021,
+                                        12,
+                                        31,
+                                        tzinfo=pytz.timezone("Asia/Calcutta")),
+                                ),
+                                (
+                                    datetime.date(2022, 12, 31),
+                                    datetime.date(2023, 12, 31),
+                                ),
+                            ))
+            #
+            results = cursor.fetchall()
+            x: Range
+            for result in results:
+                for x in result:
+                    print(x,
+                          x.lower,
+                          x.upper, (x.lower_inc, x.upper_inc),
+                          sep=" ░▒▓█▓▒░ ")
+                    # print(type(x))
+                    # print(dir(x))
+                    print()
+                print()
+                print()
+            print()
+            #
+            cursor.execute("drop table if exists demo_arrays;")
+            #
+        connection.commit()
+        connection.close()
+    pass
+
+
+if __name__ == "__main__":
+    main()
+    # help(Range)
+    pass
